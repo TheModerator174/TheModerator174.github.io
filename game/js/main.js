fdf1c308e53d6ffae302dc8b1a1d32dca0629117
Carlos Banuelos <carlosbanuelos174@gmail.com>
2017-07-17 12:46:43 -0400

    speed increase

--- game/js/main.js
+++ game/js/main.js
@@ -24,7 +24,7 @@ Hero.prototype = Object.create(Phaser.Sprite.prototype);
 Hero.prototype.constructor = Hero;
 
 Hero.prototype.move = function (direction) {
-    const SPEED = 200;
+    const SPEED = 900;
     this.body.velocity.x = direction * SPEED;
 
     // update image flipping & animations
